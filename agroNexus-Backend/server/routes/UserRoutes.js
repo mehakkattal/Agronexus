2ec48c8539d9690eb561f7fb1f0831f850f5a6dc
mehakkattal <kr9453370@gmail.com>
2026-05-20 18:49:15 +0530

    small  change

--- agroNexus-Backend/server/routes/UserRoutes.js
+++ agroNexus-Backend/server/routes/UserRoutes.js
@@ -1,26 +1,75 @@
-const express=require("express")
-const router=express.Router()
-const UserController=require("../apis/User/UserController")
-const SeasonController=require("../apis/Season/SeasonController")
-const LandController=require("../apis/Land/LandController")
-const CropController=require("../apis/Crop/CropController")
-const ProgressController=require("../apis/Progress/ProgressController")
-const BookingController=require("../apis/Booking/BookingController")
-// const mailController= require("../apis/mail/mailController")
+const express = require("express")
+const router = express.Router()
+
+const UserController = require("../apis/User/UserController")
+const SeasonController = require("../apis/Season/SeasonController")
+const LandController = require("../apis/Land/LandController")
+const CropController = require("../apis/Crop/CropController")
+const ProgressController = require("../apis/Progress/ProgressController")
+const BookingController = require("../apis/Booking/BookingController")
+// const mailController = require("../apis/mail/mailController")
 
 
-router.post("/user/login",UserController.login)
+// LOGIN
+router.post("/user/login", UserController.login)
 
 
+// TOKEN CHECKER
 router.use(require("../middleware/UserTokenChecker"))
 
 
-router.post("/user/booking/add",BookingController.add)
-router.post("/user/booking/update",BookingController.update)
-router.post("/user/booking/delete",BookingController.Delete)
+// BOOKING ROUTES
+router.post("/user/booking/add", BookingController.add)
+
+router.post("/user/booking/update", BookingController.update)
+
+router.post("/user/booking/delete", BookingController.Delete)
+
+
 // router.post("/sendMail", mailController.mail)
 
+module.exports = router
 
 
-module.exports=router
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express=require("express")
+// const router=express.Router()
+// const UserController=require("../apis/User/UserController")
+// const SeasonController=require("../apis/Season/SeasonController")
+// const LandController=require("../apis/Land/LandController")
+// const CropController=require("../apis/Crop/CropController")
+// const ProgressController=require("../apis/Progress/ProgressController")
+// const BookingController=require("../apis/Booking/BookingController")
+// // const mailController= require("../apis/mail/mailController")
+
+
+// router.post("/user/login",UserController.login)
+
+
+// router.use(require("../middleware/UserTokenChecker"))
+
+
+// router.post("/user/booking/add",BookingController.add)
+// router.post("/user/booking/update",BookingController.update)
+// // router.post("/sendMail", mailController.mail)
+
+
+
+// module.exports=router
 
