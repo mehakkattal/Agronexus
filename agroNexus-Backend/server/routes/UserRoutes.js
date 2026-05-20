6e03765c8d6d52678a6a3437488d369b32ed4728
mehakkattal <kr9453370@gmail.com>
2026-05-20 19:57:53 +0530

    small change

--- agroNexus-Backend/server/routes/UserRoutes.js
+++ agroNexus-Backend/server/routes/UserRoutes.js
@@ -11,7 +11,7 @@ const BookingController = require("../apis/Booking/BookingController")
 
 
 // LOGIN
-router.post("/user/login", UserController.login)
+router.post("/login", UserController.login)
 
 
 // TOKEN CHECKER
@@ -19,16 +19,67 @@ router.use(require("../middleware/UserTokenChecker"))
 
 
 // BOOKING ROUTES
-router.post("/user/booking/add", BookingController.add)
+router.post("/booking/add", BookingController.add)
 
-router.post("/user/booking/update", BookingController.update)
+router.post("/booking/update", BookingController.update)
 
-router.post("/user/booking/delete", BookingController.Delete)
+router.post("/booking/delete", BookingController.Delete)
 
 
 // router.post("/sendMail", mailController.mail)
 
 module.exports = router
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require("express")
+// const router = express.Router()
+
+// const UserController = require("../apis/User/UserController")
+// const SeasonController = require("../apis/Season/SeasonController")
+// const LandController = require("../apis/Land/LandController")
+// const CropController = require("../apis/Crop/CropController")
+// const ProgressController = require("../apis/Progress/ProgressController")
+// const BookingController = require("../apis/Booking/BookingController")
+// // const mailController = require("../apis/mail/mailController")
+
+
+// // LOGIN
+// router.post("/user/login", UserController.login)
+
+
+// // TOKEN CHECKER
+// router.use(require("../middleware/UserTokenChecker"))
+
+
+// // BOOKING ROUTES
+// router.post("/user/booking/add", BookingController.add)
+
+// router.post("/user/booking/update", BookingController.update)
+
+// router.post("/user/booking/delete", BookingController.Delete)
+
+
+// // router.post("/sendMail", mailController.mail)
+
+// module.exports = router
 
 
 
