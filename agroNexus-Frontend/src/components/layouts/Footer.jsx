b292c7092797e442bc2f4aef9bfa84e6d8060f67
mehakkattal <kr9453370@gmail.com>
2026-05-27 02:07:44 +0530

    small change

--- agroNexus-Frontend/src/components/layouts/Footer.jsx
+++ agroNexus-Frontend/src/components/layouts/Footer.jsx
@@ -115,7 +115,7 @@ function Footer(){
         <div className="col-md-6 text-center text-md-start mb-3 mb-md-0">
           ©{" "}
           <a className="border-bottom" href="#">
-            Your Site Name
+            YAmrita
           </a>
           , All Right Reserved.
         </div>
